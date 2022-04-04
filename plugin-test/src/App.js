749d863b50de4f8355834ccc9162415ecc189980
Gustavo Gomez <gustavogomez092@gmail.com>
2022-04-04 17:18:26 -0600

    test plugin

---
+++ plugin-test/src/App.js
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App () {
+  return (
+    <div className='App'>
+      <h1 className='text-[80px] font-bold underline decoration-red-700'>
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App
