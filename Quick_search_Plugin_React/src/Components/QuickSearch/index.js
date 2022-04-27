25eb26e01afddf6855475054f7fd648773918172
Miguel Espinales <mee@topfloormarketing.net>
2022-04-27 14:59:36 -0600

    fix css

--- Quick_search_Plugin_React/src/Components/QuickSearch/index.js
+++ Quick_search_Plugin_React/src/Components/QuickSearch/index.js
@@ -7,20 +7,20 @@ import TabsQuickSearch from '../TabsQuickSearch'
 
 const QuickSearch = () => {
     return (
-        <div className='w-full h-full'>
-            <Card className='py-8'>
-                <div className='grid grid-cols-1 gap-4 md:grid-cols-2'>
-                    <div className='flex items-center justify-center w-full h-full mx-5 md:mx-0'>
-                        <h3 className='font-normal text-left text-[30px] xl:text-[40px] leading-10 w-full lg:w-1/2'>
-                            Search for new properties:
-                        </h3>
-                    </div>
-                    <div className='mx-5'>
-                        <TabsQuickSearch />
-                    </div>
+        <Card className='py-8 bg-[#fffffff2]'>
+            <div className='grid grid-cols-1 gap-4 md:grid-cols-2'>
+                <div className='flex items-center justify-center w-full h-full px-5'>
+                    <h3 className='font-normal text-left text-[30px] xl:text-[40px] leading-10 w-full'>
+                        Search for new 
+                        <br />
+                        properties:
+                    </h3>
                 </div>
-            </Card>
-        </div>
+                <div className='px-5'>
+                    <TabsQuickSearch />
+                </div>
+            </div>
+        </Card>
     )
 }
 
