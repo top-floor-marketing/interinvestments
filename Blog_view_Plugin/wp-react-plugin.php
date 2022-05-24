0829fb35407d270b14e6c373b59d100fee160bf0
Miguel Espinales <mee@topfloormarketing.net>
2022-05-24 10:03:03 -0600

    first plugin commit

---
+++ Blog_view_Plugin/wp-react-plugin.php
@@ -0,0 +1,57 @@
+<?php
+/*
+Plugin Name: Blog_view
+Description: Use the [Blog_view] shortcode to display the plugin
+Version: 0.0.1
+Author: Topfloormarketing
+Author URI: https://www.topfloormarketing.net/
+*/
+
+class Blog_view {
+
+    protected $plugin_options_page = '';
+
+    /**
+     * Class constructor
+     */
+    public function __construct() {
+      require('plugin_options.php');
+    }
+
+    /**
+     * Initialize hooks.
+     */
+    public function init() {
+
+      add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_frontend_scripts' ) );
+    }
+
+    public function enqueue_frontend_scripts($hook) {
+
+      //wp_enqueue_script('react');
+      //wp_enqueue_script('react-dom');
+      
+      // add react and react-dom from core
+      $dep = ''; //['wp-element'];
+    }
+}
+
+$Blog_view = new Blog_view();
+$Blog_view->init();
+
+function Blog_view_shortcode( $atts ) {
+  $handle = 'blog-view-wp-react-plugin-';
+
+  // enqueue development or production React code
+  if(file_exists(dirname(__FILE__) . "/dist/static/js/main.js")) {
+    $handle .= 'prod';
+    wp_enqueue_script( $handle, plugins_url( "/dist/static/js/main.js", __FILE__ ), ['wp-element'], '0.1', true );
+    wp_enqueue_style( $handle, plugins_url( "/dist/static/css/main.css", __FILE__ ), false, '0.1', 'all' );
+  } else {
+    $handle .= 'dev';
+    wp_enqueue_script( $handle, 'http://localhost:3000/static/js/bundle.js', ['wp-element'], '0.1', true );
+  }
+  return "<div id='Blog_view'></div>";
+}
+
+add_shortcode( 'Blog_view', 'Blog_view_shortcode' );
