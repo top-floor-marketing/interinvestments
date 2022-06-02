4e79f5bd85dce52b16e7617f4ab7c2e62bfa67ca
Marcos Esp <marcos.ee16@gmail.com>
2022-06-02 11:25:51 -0600

    profile layout

--- CRMDashboard/src/Component/Profile/index.js
+++ CRMDashboard/src/Component/Profile/index.js
@@ -1,7 +1,36 @@
 import { Box, createStyles } from "@mantine/core";
+import { useSpring, animated } from "react-spring";
+
+const useStyles = createStyles((theme, _params, getRef) => ({
+  boxContainer: {
+    display: "grid",
+    gridTemplateRows: "1fr 1fr",
+    gap: theme.other.spacing.p5,
+  },
+  boxGrid2: {
+    display: "grid",
+    gap: theme.other.spacing.p5,
+    gridTemplateColumns: "1fr 1fr",
+  },
+}));
 
 const Profile = () => {
-  return <Box>PROFILE</Box>;
+  const { classes } = useStyles();
+  const animateProps = useSpring({
+    to: { opacity: 1 },
+    from: { opacity: 0 },
+    reset: false,
+    delay: 200,
+    config: { duration: 1000 },
+  });
+  return (
+    <animated.div style={animateProps}>
+      <Box className={classes.boxContainer}>
+        <Box></Box>
+        <Box className={classes.boxGrid2}></Box>
+      </Box>
+    </animated.div>
+  );
 };
 
 export default Profile;
