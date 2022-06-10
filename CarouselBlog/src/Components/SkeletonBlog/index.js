37e26f9b8c8dc35d16c2eefdd09c242230b384af
MSocram16 <marcos.ee16@gmail.com>
2022-06-09 21:20:14 -0600

    merge

--- CarouselBlog/src/Components/SkeletonBlog/index.js
+++ CarouselBlog/src/Components/SkeletonBlog/index.js
@@ -1,10 +1,6 @@
 import { Skeleton } from "@mantine/core";
 
-<<<<<<< HEAD
-import styles from "./blog_grid_styles.module.scss";
-=======
 import styles from "./styles.cb.module.scss";
->>>>>>> e695d59bd466a9d4a55fa894ee3a3ee47ec9fc42
 
 const SkeletonBlog = () => {
   return (
