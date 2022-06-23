4e90d35bcec6c2a79d1dc821e9958403482892c2
Miguel Espinales <mee@topfloormarketing.net>
2022-06-23 12:48:25 -0600

    fix gql

--- Single_Listing_Viewer/src/GraphqlClient/GQL.js
+++ Single_Listing_Viewer/src/GraphqlClient/GQL.js
@@ -2,7 +2,7 @@ import { gql } from "graphql-request";
 
 export const LISTINGS_BY_SLOG = gql`
   query listingsBySlog($title: String!) {
-    listings(where: {title: $title}) {
+    listings(where: {title: $title}, first: 1) {
       nodes {
         databaseId
         address {
