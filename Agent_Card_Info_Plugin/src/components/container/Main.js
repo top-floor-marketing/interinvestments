485ecbd7aa26e04cb0e7921fbf1153037856600c
Miguel Espinales <mee@topfloormarketing.net>
2022-07-08 10:12:07 -0600

    firs commit

---
+++ Agent_Card_Info_Plugin/src/components/container/Main.js
@@ -0,0 +1,17 @@
+import React from 'react'
+// components
+
+// mantine
+import { Box } from '@mantine/core';
+// styles
+import style from '../../blogStyles.module.scss'
+
+const Main = () => {
+    return (
+        <Box className={style.containerMain}>
+            <p>Main</p>
+        </Box>
+    )
+}
+
+export default Main
