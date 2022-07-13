47ae099dd1732ba22c3ea68b39a39fb545f0e33d
Miguel Espinales <mee@topfloormarketing.net>
2022-07-13 14:15:01 -0600

    first commit

---
+++ All_Listing_View_Plugin/src/components/container/Main.js
@@ -0,0 +1,17 @@
+import React from 'react'
+// components
+
+// mantine
+import { Box } from '@mantine/core';
+// styles
+import style from '../../blogStyles.module.scss'
+
+const Main = () => {
+    return (
+        <Box className={style.containerMain}>
+            <p>All_Listing_View</p>
+        </Box>
+    )
+}
+
+export default Main
