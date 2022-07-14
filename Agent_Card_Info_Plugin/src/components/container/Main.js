8dc5f887ca300bbb94185dee589443ad7dbe4d35
Miguel Espinales <mee@topfloormarketing.net>
2022-07-14 16:23:37 -0600

    fix scss

--- Agent_Card_Info_Plugin/src/components/container/Main.js
+++ Agent_Card_Info_Plugin/src/components/container/Main.js
@@ -4,11 +4,11 @@ import CardAgent from '../CardAgent'
 // mantine
 import { Box, Text } from '@mantine/core';
 // styles
-import style from '../../blogStyles.module.scss'
+import style from '../../blogStyles.CIP.module.scss'
 
 const Main = () => {
     return (
-        <Box className={style.containerMain}>
+        <Box className={style.containerMainAgentCarInfo}>
             <Box className={style.containerCard}>
                 <Box className={style.titleCard}>
                     <Text component='h4'>Your agent</Text>
