a8a3d5aad11ddbf71c21eb3f7445cda8482f1b89
Miguel Espinales <mee@topfloormarketing.net>
2022-07-28 15:23:46 -0600

    validation pin in map component

--- All_Listing_View_Plugin/src/components/MapListing/Marker.js
+++ All_Listing_View_Plugin/src/components/MapListing/Marker.js
@@ -1,48 +1,97 @@
 import React, { useState, useRef } from 'react'
 //mantine
-import { Box, Image, HoverCard, Text } from '@mantine/core';
+import { Box, Image, HoverCard, Text, Avatar } from '@mantine/core';
+import { useMediaQuery } from '@mantine/hooks';
 // assets
 import imagePin from './asset/Pin.svg'
 // styles 
 import style from './styles.ML.module.scss'
 
 const Marker = (props) => {
-    const { lat, lng } = props
+    const { lat, lng, title, subTitle, price, urlImagen, uri } = props
     const [opened, setOpened] = useState(false);
     const refImagen = useRef(null);
-    return (
-        <Box
-            lat={lat}
-            lng={lng}
-            className={style.containerMarker}
-        >
-            <HoverCard
-                width={280}
-                shadow="md"
-                position='right'
+    const matches = useMediaQuery('(min-width: 1024px)');
+
+    if (lat && lng) {
+        return (
+            <Box
+                lat={lat}
+                lng={lng}
+                className={`${style.containerMarker} group`}
             >
-                <HoverCard.Target>
-                    <Image
-                        ref={refImagen}
-                        onClick={() => setOpened(!opened)}
-                        width={28}
-                        height={35}
-                        radius={null}
-                        src={imagePin}
-                        alt="PinMap_"
-                    />
-                </HoverCard.Target>
-                <HoverCard.Dropdown
-                    className='!cursor-auto'
+                <HoverCard
+                    width={!matches ? 217 : 340}
+                    shadow="md"
+                    position={matches ? 'right' : 'bottom'}
+                    radius='lg'
+                    opened={opened}
                 >
-                    <Text size="sm">
-                        Hover card is revealed when user hovers over target element, it will be hidden once
-                        mouse is not over both target and dropdown elements
-                    </Text>
-                </HoverCard.Dropdown>
-            </HoverCard>
-        </Box>
-    )
+                    <HoverCard.Target>
+                        <Image
+                            className={`${style.imagenPin} group-hover:opacity-100 ${opened ? '!opacity-100' : ''}`}
+                            ref={refImagen}
+                            onClick={() => setOpened(!opened)}
+                            width={28}
+                            height={35}
+                            radius={null}
+                            src={imagePin}
+                            alt={`PinMap_${title}`}
+                        />
+                    </HoverCard.Target>
+                    <HoverCard.Dropdown
+                        className='!cursor-grab px-3'
+                    >
+                        <Box className='flex flex-col gap-5 lg:flex-row'>
+                            <Avatar
+                                className={style.avatarListing}
+                                radius="xs"
+                                alt={`ImagenListing_${title}`}
+                                src={urlImagen}
+                            />
+                            <Box className='flex flex-col justify-between'>
+                                {
+                                    title && (
+                                        <Text
+                                            title={title}
+                                            className={style.titleListingMap}
+                                            lineClamp={1}
+                                            component='a'
+                                            href={uri}
+                                        >
+                                            {title}
+                                        </Text>
+                                    )
+                                }
+                                <Box className={style.containerInfoListing}>
+                                    {
+                                        subTitle && (
+                                            <Text
+                                                title={subTitle}
+                                                component='span'
+                                                lineClamp={1}
+                                            >
+                                                {subTitle}
+                                            </Text>
+                                        )
+                                    }
+                                    {
+                                        price && (
+                                            <Text title={`Price ${price}`} component='span' lineClamp={1}>
+                                                Price {price}
+                                            </Text>
+                                        )
+                                    }
+                                </Box>
+                            </Box>
+                        </Box>
+                    </HoverCard.Dropdown>
+                </HoverCard>
+            </Box>
+        )
+    } else {
+        return null
+    }
 }
 
 export default Marker
--- All_Listing_View_Plugin/src/components/MapListing/index.js
+++ All_Listing_View_Plugin/src/components/MapListing/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 //mantine
 import { Box, Overlay } from '@mantine/core';
 // components
@@ -12,16 +12,22 @@ import { useSelector } from 'react-redux'
 import style from './styles.ML.module.scss'
 
 const MapListing = (props) => {
+    const [defaultPropsMap, setDefaultProps] = useState({
+        lat: 25.761681,
+        lng: -80.191788
+    })
     const { isLoading } = props
-    const { mapApiKey } = useSelector((state) => state.statusQuery)
+    const { mapApiKey, dataListing } = useSelector((state) => state.statusQuery)
 
-    const defaultProps = {
-        center: {
-            lat: 25.761681,
-            lng: -80.191788
-        },
-        zoom: 11
-    };
+    useEffect(() => {
+        if (dataListing.length) {
+            const { latitude, longitude } = dataListing[0].listingData.newDevelopment
+            setDefaultProps({
+                lat: parseFloat(latitude),
+                lng: parseFloat(longitude)
+            })
+        }
+    }, [dataListing])
 
     return (
         <Box className='relative w-full h-full'>
@@ -40,13 +46,27 @@ const MapListing = (props) => {
                     styles: stylesmaps
                 }}
                 bootstrapURLKeys={{ key: mapApiKey }}
-                defaultCenter={defaultProps.center}
-                defaultZoom={defaultProps.zoom}
+                defaultCenter={defaultPropsMap}
+                defaultZoom={11}
             >
-                <Marker
-                    lat={25.761681}
-                    lng={-80.191788}
-                />
+                {
+                    dataListing.map((value, index) => {
+                        const { latitude, longitude, priceMin = 0, priceMax = 0, photos } = value.listingData.newDevelopment
+                        return (
+                            <Marker
+                                uri={value.uri}
+                                title={value.title}
+                                subTitle={value.neighborhoods.nodes[0]?.name}
+                                price={`$${priceMin} - $${priceMax}`}
+                                key={index}
+                                lat={latitude}
+                                lng={longitude}
+                                urlImagen={photos[0]?.sourceUrl}
+                            />
+                        )
+                    })
+                }
+
             </GoogleMapReact>
 
         </Box>
