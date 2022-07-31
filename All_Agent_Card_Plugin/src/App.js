08f7aa360d5d15dc0183046091ae4e86e0e8015f
Miguel Espinales <mee@topfloormarketing.net>
2022-07-31 15:07:10 -0600

    fisrt commit plugin

---
+++ All_Agent_Card_Plugin/src/App.js
@@ -0,0 +1,26 @@
+import { useEffect } from "react";
+// components
+import Main from './components/container/Main'
+// react query
+import { QueryClient, QueryClientProvider } from "react-query";
+// animation aos
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+
+function App() {
+  const queryClient = new QueryClient();
+  useEffect(() => {
+    AOS.init();
+    AOS.refresh();
+  }, []);
+  return (
+    <div className='App'>
+      <QueryClientProvider client={queryClient}>
+        <Main />
+      </QueryClientProvider>
+    </div>
+  )
+}
+
+export default App
---
+++ All_Agent_Card_Plugin/src/components/container/Main.js
@@ -0,0 +1,17 @@
+import React from 'react'
+// components
+
+// mantine
+import { Box } from '@mantine/core';
+// styles
+import styles from './blogStyles.CM.module.scss'
+
+const Main = () => {
+    return (
+        <Box className={styles.containerMain}>
+            <p>All AGent Card</p>
+        </Box>
+    )
+}
+
+export default Main
