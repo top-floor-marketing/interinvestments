432b07048130fbb1cc0f74d1cb4ee829dbd54073
Miguel Espinales <mee@topfloormarketing.net>
2022-08-16 13:48:13 -0600

    add input in form step one

--- CRMDashboard/src/Component/DrawerAddLeads/Steps/LeadsInfo.js
+++ CRMDashboard/src/Component/DrawerAddLeads/Steps/LeadsInfo.js
@@ -1,17 +1,23 @@
 import React from 'react'
 // mantine
-import { Box, createStyles, TextInput } from '@mantine/core';
+import { Box, createStyles, TextInput, Textarea } from '@mantine/core';
 // icons
 import { User, Mail } from 'tabler-icons-react';
 
 const useStyles = createStyles((theme, _params) => ({
     containerMain: {
-        display: 'flex',
-        flexDirection: 'row',
+        display: "grid",
+        gridTemplateColumns: "repeat(1, minmax(0, 1fr))",
+        [`@media (min-width: ${theme.breakpoints.md}px)`]: {
+            gridTemplateColumns: "repeat(2, minmax(0, 1fr))",
+        },
         gap: theme.other.spacing.p4
     },
     input: {
         width: '100%'
+    },
+    inputFull: {
+        gridColumn: "1 / -1",
     }
 }));
 
@@ -30,6 +36,11 @@ const LeadsInfo = () => {
                 placeholder="Email client"
                 label="Email client"
                 rightSection={<Mail />}
+            />
+            <Textarea
+                className={`${classes.input} ${classes.inputFull}`}
+                placeholder="your note leads..."
+                label="Note"
             />
         </Box>
     )
