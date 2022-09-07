9f0eacfbe2664dcda1096380b6babede680b7659
Miguel Espinales <mee@topfloormarketing.net>
2022-09-07 14:55:56 -0600

    add view pipeline

---
+++ CRMDashboard/src/Container/PipelineView/PipelineItem.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const PipelineItem = (props) => {
+    console.log('PipelineItem_Props', props)
+    return (
+        <div>PipelineItem</div>
+    )
+}
+
+export default PipelineItem
