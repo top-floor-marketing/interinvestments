12a877258a1dd04f60383ecf12dc8d4920c7bb33
MSokram16 <marcos.ee16@gmail.com>
2022-09-13 12:09:10 -0600

    build plugins

--- ShareAgentPlugin/src/App.js
+++ ShareAgentPlugin/src/App.js
@@ -11,6 +11,8 @@ const URL_SHARED_FLAG = 'shared';
 const URL_QUERY_ID_NAME = 'agent-id';
 const LOCASTORAGE_ID_NAME = 'lead-agent';
 const LOCASTORAGE_AGENT_LAST_DATE = 'agent-last-date-url-shared';
+const TOKEN_CRM = 'crm-token';
+const USER_CRM = "crm-user-id";
 
 dayjs.extend(utc);
 
@@ -25,16 +27,25 @@ function App() {
     const isShared = (queryParams.get(URL_SHARED_FLAG).toLowerCase() === 'true');
     const idInUrl = toInteger(queryParams.get(URL_QUERY_ID_NAME));
     let idInLocal = toInteger(localStorage.getItem(LOCASTORAGE_ID_NAME));
+    let tokenCrm = localStorage.getItem(TOKEN_CRM);
+    let userCrm = localStorage.getItem(USER_CRM);
+
+    const isAgentUser = (tokenCrm && userCrm);
+    const isValidUrl = (isShared && idInUrl);
 
     if (isShared && idInUrl) {
       Array.from(document.querySelectorAll("#menu-primary-menu>.menu-item>a")).map((x) => {
         const text = toLower(x.textContent || x.innerText);
-        return x.href = (text !== "login") ? x.href + `?${URL_QUERY_ID_NAME}=${idInUrl}&${URL_SHARED_FLAG}=true` : x.href
+        return {
+          ...x,
+          ref: (text !== "login") ? x.href + `?${URL_QUERY_ID_NAME}=${idInUrl}&${URL_SHARED_FLAG}=true` : x.href,
+          innerText: (text === "agents" && (isAgentUser || isValidUrl)) ? 'My Bio' : x.innerText
+        }
       }
       );
     }
 
-    if (!isEqual(idInUrl, idInLocal) && idInUrl && isShared) {
+    if (!isEqual(idInUrl, idInLocal) && isValidUrl) {
       localStorage.setItem(LOCASTORAGE_ID_NAME, idInUrl);
       localStorage.setItem(LOCASTORAGE_AGENT_LAST_DATE, currentUtc);
     }
