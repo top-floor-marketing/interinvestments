a9f6324ec0a9be45660f1f25c50db2a5cf9adf36
Marcos Esp <marcos.ee16@gmail.com>
2022-09-19 13:01:16 -0600

    build crm

--- CRMDashboard/src/Component/PdfListener/eventsListener.js
+++ CRMDashboard/src/Component/PdfListener/eventsListener.js
@@ -3,9 +3,8 @@ const PDF_LISTING_LISTENER = process.env.REACT_APP_PDF_LISTING_LISTENER;
 const triggerPdfListing = ({ idListing = null, idAgent = null }) => {
   const eventType = PDF_LISTING_LISTENER;
   if(!eventType || !idListing) return null;
-  console.log("Trigger ", eventType);
   console.log("values ", { idListing, idAgent });
-  const event = new CustomEvent(eventType, { data: { idListing, idAgent } });
+  const event = new CustomEvent(eventType, { detail: { idListing, idAgent } });
   document.dispatchEvent(event);
 }
 
