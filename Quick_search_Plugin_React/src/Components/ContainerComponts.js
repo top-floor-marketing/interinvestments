387f0ab3082f9ba181496ca68e62d5e028155200
Miguel Espinales <mee@topfloormarketing.net>
2022-10-06 15:51:05 -0600

    fix position

--- Quick_search_Plugin_React/src/Components/ContainerComponts.js
+++ Quick_search_Plugin_React/src/Components/ContainerComponts.js
@@ -8,21 +8,21 @@ import MenuQuickSearch from '../Components/MenuQuickSearch'
 import useStore from '../Store/useStore';
 // utils
 import { useWindowScroll, useHotkeys } from '@mantine/hooks';
-import useWindowDimensions from '../hook/useWindowDimensions.'
 
 const ContainerComponts = (props) => {
-    const { height } = useWindowDimensions();
     const containerRef = useRef(null);
     const [{ y }] = useWindowScroll();
     const { state: { focusMenu }, setFocusMenu } = useStore();
 
 
     useEffect(() => {
-        if (y > (height + containerRef?.current?.clientHeight) && focusMenu) {
-            console.log('setFocusMenu : false')
-            setFocusMenu(false)
+        if (focusMenu) {
+            const boxContainer = document.querySelector("#boxContainerSearch");
+            if (y > (boxContainer?.parentNode?.offsetTop + containerRef?.current?.clientHeight) && focusMenu) {
+                setFocusMenu(false)
+            }
         }
-    }, [y, focusMenu, setFocusMenu, height])
+    }, [y, focusMenu, setFocusMenu])
 
     useHotkeys([
         ['Escape', () => {
@@ -32,15 +32,8 @@ const ContainerComponts = (props) => {
         }],
     ]);
 
-    console.log('useWindowScroll_Y : ', y)
-    console.log('PositionComponen_Curren : ', containerRef?.current)
-    console.log('PositionComponen_offsetTop : ', containerRef?.current?.offsetTop)
-    console.log('PositionComponen_clientHeight : ', containerRef?.current?.clientHeight)
-    console.log('PositionComponen_Y : ', containerRef?.current?.offsetTop + containerRef?.current?.clientHeight + 100)
-    console.log('screen.height', height)
-
     return (
-        <Box ref={containerRef}>
+        <Box id="boxContainerSearch" ref={containerRef}>
             <QuickSearch />
             <MenuQuickSearch />
         </Box>
