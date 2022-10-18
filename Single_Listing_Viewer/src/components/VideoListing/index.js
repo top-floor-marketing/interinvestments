0a09c4d90f28816c98269ae9cbe1c3bee01bdb9a
Mes1991 <mes@topfloormarketing.net>
2022-10-18 12:52:55 -0600

    fix single

--- Single_Listing_Viewer/src/components/VideoListing/index.js
+++ Single_Listing_Viewer/src/components/VideoListing/index.js
@@ -8,15 +8,14 @@ import styles from './styles.vl.module.scss'
 
 const VideoListing = (props) => {
     const { data } = props
-
+ // https://interinvestments.bytfm.com/wp-content/uploads/2022/04/Cinematic-Real-estate-video-tour-example-4K-Laowa-12mm-Sony-A7III.mp4
     if (!data?.video)
          return null
     return (
         <Box className={styles.containerVideoListing}>
             <div className={styles.itemVideo}>
             <ReactPlayer
-                controls
-                light
+               
                 width={"100%"}
                 heigth={"100%"}
                 url={data.video.mediaItemUrl}
