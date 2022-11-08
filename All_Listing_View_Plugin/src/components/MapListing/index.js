3923ad6cc23a9005784036d47a2d9c4b87ef4792
Miguel-Espinales-ESES <mee@topfloormarketing.net>
2022-11-08 15:34:52 -0600

    Fix map

--- All_Listing_View_Plugin/src/components/MapListing/index.js
+++ All_Listing_View_Plugin/src/components/MapListing/index.js
@@ -1,75 +1,78 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 //mantine
-import { Box, LoadingOverlay } from '@mantine/core';
+import { Box, LoadingOverlay } from "@mantine/core";
 // components
-import Marker from './Marker'
+import Marker from "./Marker";
 // map
-import GoogleMapReact from 'google-map-react';
-import stylesmaps from './stylesmaps'
+import GoogleMapReact from "google-map-react";
+import stylesmaps from "./stylesmaps";
 // redux
-import { useSelector } from 'react-redux'
-// styles 
-import style from './styles.ML.module.scss'
+import { useSelector } from "react-redux";
+// styles
+import style from "./styles.ML.module.scss";
 
 const MapListing = (props) => {
-    const [defaultPropsMap, setDefaultProps] = useState({
-        lat: 25.761681,
-        lng: -80.191788
-    })
-    const { isLoading } = props
-    const { mapApiKey, dataListing } = useSelector((state) => state.statusQuery);
+  const [defaultPropsMap, setDefaultProps] = useState({
+    lat: 25.761681,
+    lng: -80.191788,
+  });
+  const { isLoading } = props;
+  const { mapApiKey, dataListing } = useSelector((state) => state.statusQuery);
 
-    useEffect(() => {
-        if (dataListing.length) {
-            const { latitude, longitude } = dataListing[0].listingData.newDevelopment
-            setDefaultProps({
-                lat: parseFloat(latitude),
-                lng: parseFloat(longitude)
-            })
-        }
-    }, [dataListing])
+  useEffect(() => {
+    if (dataListing.length) {
+      const { latitude, longitude } = dataListing[0].listingData.newDevelopment;
+      setDefaultProps({
+        lat: parseFloat(latitude),
+        lng: parseFloat(longitude),
+      });
+    }
+  }, [dataListing]);
 
-    return (
-        <Box className='relative w-full h-full'>
-                    <LoadingOverlay
-                        loaderProps={{ size: 'sm', color: '#FFB839', variant: 'bars' }}
-                        visible={isLoading}
-                        className={style.overlayMapListing}
-                        zIndex={100}
-                        overlayOpacity={0.2}
-                        overlayColor="#f5f6fa"
-                        transitionDuration={500}
-                        overlayBlur={0.5}
-                    />
-         {/*  <GoogleMapReact
-                options={{
-                    styles: stylesmaps
-                }}
-                bootstrapURLKeys={{ key: mapApiKey }}
-                defaultCenter={defaultPropsMap}
-                defaultZoom={11}
-            >
-                {
-                    dataListing.map((value, index) => {
-                        const { latitude, longitude, priceMin = 0, priceMax = 0, photos } = value.listingData.newDevelopment
-                        return (
-                            <Marker
-                                uri={value.uri}
-                                title={value.title}
-                                subTitle={value.neighborhoods.nodes[0]?.name}
-                                price={`$${priceMin} - $${priceMax}`}
-                                key={index}
-                                lat={latitude}
-                                lng={longitude}
-                                urlImagen={photos[0]?.sourceUrl}
-                            />
-                        )
-                    })
-                }
+  return (
+    <Box className="relative w-full h-full">
+      <LoadingOverlay
+        loaderProps={{ size: "sm", color: "#FFB839", variant: "bars" }}
+        visible={isLoading}
+        className={style.overlayMapListing}
+        zIndex={100}
+        overlayOpacity={0.2}
+        overlayColor="#f5f6fa"
+        transitionDuration={500}
+        overlayBlur={0.5}
+      />
+      <GoogleMapReact
+        options={{
+          styles: stylesmaps,
+        }}
+        bootstrapURLKeys={{ key: mapApiKey }}
+        defaultCenter={defaultPropsMap}
+        defaultZoom={11}
+      >
+        {dataListing.map((value, index) => {
+          const {
+            latitude,
+            longitude,
+            priceMin = 0,
+            priceMax = 0,
+            photos,
+          } = value.listingData.newDevelopment;
+          return (
+            <Marker
+              uri={value.uri}
+              title={value.title}
+              subTitle={value.neighborhoods.nodes[0]?.name}
+              price={`$${priceMin} - $${priceMax}`}
+              key={index}
+              lat={latitude}
+              lng={longitude}
+              urlImagen={photos[0]?.sourceUrl}
+            />
+          );
+        })}
+      </GoogleMapReact>
+    </Box>
+  );
+};
 
-            </GoogleMapReact> */}
-        </Box>
-    )
-}
-
-export default MapListing
+export default MapListing;
--- All_Listing_View_Plugin/src/components/container/Main.js
+++ All_Listing_View_Plugin/src/components/container/Main.js
@@ -1,116 +1,117 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 // components
-import FiltersListings from '../FiltersListings'
-import { SkeletonGrid, LoaderMaps } from '../LoadingListing'
-import ModalQuickView from '../ModalQuickView'
-import MapListing from '../MapListing'
-import GridListing from '../GridListing'
-import AlertError from '../AlertError'
+import FiltersListings from "../FiltersListings";
+import { SkeletonGrid, LoaderMaps } from "../LoadingListing";
+import ModalQuickView from "../ModalQuickView";
+import MapListing from "../MapListing";
+import GridListing from "../GridListing";
+import AlertError from "../AlertError";
 // Hooks
-import { useGetFeaturedDev } from '../../Hooks'
+import { useGetFeaturedDev } from "../../Hooks";
 // mantine
-import { Box, Header, AppShell, LoadingOverlay } from '@mantine/core';
+import { Box, Header, AppShell, LoadingOverlay } from "@mantine/core";
 // styles
-import style from '../../styles.ALV.module.scss'
+import style from "../../styles.ALV.module.scss";
 
 const Main = () => {
-    const [idSingleListing, setIdSingleListing] = useState(null)
-    // usar hook, validar error o skeleton
-    const { isSkeleton, isError, refetchListing, dataListing, totalData, loadingListing } = useGetFeaturedDev()
+  const [idSingleListing, setIdSingleListing] = useState(null);
+  // usar hook, validar error o skeleton
+  const {
+    isSkeleton,
+    isError,
+    refetchListing,
+    dataListing,
+    totalData,
+    loadingListing,
+  } = useGetFeaturedDev();
 
-    if (isError) {
-        return (
-            <Box className='flex items-center justify-center w-full h-screen'>
-                <AlertError
-                    label='Error!'
-                    description='Please wait a few minutes before you try again'
-                />
-            </Box>
-        )
-    }
-
-    const onOpenModal = (idListing) => {
-        setIdSingleListing(idListing)
-    }
-
-    const onCloseModal = () => {
-        setIdSingleListing(null)
-    }
-
+  if (isError) {
     return (
-        <>
-            {
-                (idSingleListing) && (
-                    <ModalQuickView
-                        idSingleListing={idSingleListing}
-                        onClose={() => onCloseModal()}
-                    />
-                )
-            }
-            <AppShell
-                className={style.containerMain}
-                classNames={{
-                    main: 'p-[20px] pb-0 lg:pl-[0px] lg:pr-0 lg:pt-0'
-                }}
-                header={
-                    <Header fixed
-                        classNames={{
-                            root: '!sticky'
-                        }}
-                    >
-                        <FiltersListings />
-                    </Header>
-                }
-            >
-                <Box className='relative'>
-                    <Box className={style.containerContend}>
-                        {
-                            (isSkeleton) ? (
-                                <Box className={style.containerGridCard}>
-                                    <SkeletonGrid />
-                                </Box>
-                            ) : (
-                                <Box
-                                    className={style.sectionGridListing}
-                                >
-                                    <LoadingOverlay
-                                        className={style.overlayGridListing}
-                                        loaderProps={{ size: 'sm', color: '#FFB839', variant: 'bars' }}
-                                        visible={loadingListing}
-                                        overlayOpacity={0.2}
-                                        overlayColor="#c5c5c5"
-                                        transitionDuration={500}
-                                        overlayBlur={0.5}
-                                    />
-                                    <GridListing
-                                        openModalQuickView={onOpenModal}
-                                        refetch={refetchListing}
-                                        data={dataListing}
-                                        totalData={totalData}
-                                        name="grid"
-                                        isLoading={loadingListing}
-                                        parentClassname={style.containerGridInfinite}
-                                    />
-                                </Box>
-                            )
-                        }
-                       <Box className={style.containerMap}>
-                            {
-                                (isSkeleton) ? (
-                                    <LoaderMaps />
-                                ) : (
-                                    <MapListing
-                                        isLoading={loadingListing}
-                                    />
-                                )
-                            }
-                        </Box>
-                    </Box>
-                </Box>
-            </AppShell>
-        </>
+      <Box className="flex items-center justify-center w-full h-screen">
+        <AlertError
+          label="Error!"
+          description="Please wait a few minutes before you try again"
+        />
+      </Box>
+    );
+  }
 
-    )
-}
+  const onOpenModal = (idListing) => {
+    setIdSingleListing(idListing);
+  };
 
-export default Main
+  const onCloseModal = () => {
+    setIdSingleListing(null);
+  };
+
+  return (
+    <>
+      {idSingleListing && (
+        <ModalQuickView
+          idSingleListing={idSingleListing}
+          onClose={() => onCloseModal()}
+        />
+      )}
+      <AppShell
+        className={style.containerMain}
+        classNames={{
+          main: "p-[20px] pb-0 lg:pl-[0px] lg:pr-0 lg:pt-0",
+        }}
+        header={
+          <Header
+            fixed
+            classNames={{
+              root: "!sticky",
+            }}
+          >
+            <FiltersListings />
+          </Header>
+        }
+      >
+        <Box className="relative">
+          <Box className={style.containerContend}>
+            {isSkeleton ? (
+              <Box className={style.containerGridCard}>
+                <SkeletonGrid />
+              </Box>
+            ) : (
+              <Box className={style.sectionGridListing}>
+                <LoadingOverlay
+                  className={style.overlayGridListing}
+                  loaderProps={{
+                    size: "sm",
+                    color: "#FFB839",
+                    variant: "bars",
+                  }}
+                  visible={loadingListing}
+                  overlayOpacity={0.2}
+                  overlayColor="#c5c5c5"
+                  transitionDuration={500}
+                  overlayBlur={0.5}
+                />
+                <GridListing
+                  openModalQuickView={onOpenModal}
+                  refetch={refetchListing}
+                  data={dataListing}
+                  totalData={totalData}
+                  name="grid"
+                  isLoading={loadingListing}
+                  parentClassname={style.containerGridInfinite}
+                />
+              </Box>
+            )}
+            <Box className={style.containerMap}>
+              {isSkeleton ? (
+                <LoaderMaps />
+              ) : (
+                <MapListing isLoading={loadingListing} />
+              )}
+            </Box>
+          </Box>
+        </Box>
+      </AppShell>
+    </>
+  );
+};
+
+export default Main;
