06260571e829b237bb15c7abbdc194f0341c3304
Miguel-Espinales-ESES <mee@topfloormarketing.net>
2022-11-08 13:32:53 -0600

    Fix Maps react v18

--- Single_Listing_Viewer/src/components/MapListing/MapComp.js
+++ Single_Listing_Viewer/src/components/MapListing/MapComp.js
@@ -1,10 +1,7 @@
 import React, { useState } from "react";
 // componet
-<<<<<<< HEAD
-import Marker from "./Marker";
-=======
 // import Marker from "./Marker";
->>>>>>> b2ebe99 (QA David)
+import Marker from "./Marker";
 //mantine
 import { Box } from "@mantine/core";
 // map
@@ -14,11 +11,7 @@ import stylesmaps from "./stylesmaps";
 import styles from "./styles.ml.module.scss";
 
 const MapComp = (props) => {
-<<<<<<< HEAD
   const [openedMarker, setOpenedMarker] = useState(false);
-=======
-  // const [openedMarker, setOpenedMarker] = useState(false);
->>>>>>> b2ebe99 (QA David)
   const { dataListing, optionTheme } = props;
   const { latitude, longitude } = dataListing;
 
@@ -28,9 +21,6 @@ const MapComp = (props) => {
       lng: parseFloat(longitude),
     },
   };
-
-<<<<<<< HEAD
-  // console.log("optionTheme", optionTheme);
 
   return (
     <Box
@@ -66,42 +56,4 @@ const MapComp = (props) => {
   );
 };
 
-=======
-  return (
-    <Box
-      className={styles.BoxMap}
-      // onMouseOver={() => setOpenedMarker(true)}
-      // onMouseOut={() => setOpenedMarker(false)}
-    >
-      <GoogleMapReact
-        options={{
-          styles: stylesmaps,
-          scrollwheel: false,
-          gestureHandling: "greedy",
-          fullscreenControl: false,
-          zoomControl: false,
-        }}
-        bootstrapURLKeys={{ key: optionTheme.mapApiKey }}
-        defaultCenter={defaultProps.center}
-        defaultZoom={15}
-      >
-        {
-          // <Marker
-          //     opened={openedMarker}
-          //     uri={dataListing.uri}
-          //     title={dataListing.title}
-          //     subTitle={dataListing.neighborhoods[0]?.name}
-          //     priceMin={dataListing.priceMin}
-          //     priceMax={dataListing.priceMax}
-          //     lat={parseFloat(latitude)}
-          //     lng={parseFloat(longitude)}
-          //     urlImagen={dataListing.photos[0]?.sourceUrl}
-          // />
-        }
-      </GoogleMapReact>
-    </Box>
-  );
-};
-
->>>>>>> b2ebe99 (QA David)
 export default MapComp;
