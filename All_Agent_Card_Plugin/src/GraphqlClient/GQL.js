6a144d5cff91cc8c91c032cd683663ffdad8d799
Msocram16 <marcos.ee16@gmail.com>
2024-11-15 09:08:42 -0600

    fixs dataAgent All_Agent_Card_Plugin

--- All_Agent_Card_Plugin/src/GraphqlClient/GQL.js
+++ All_Agent_Card_Plugin/src/GraphqlClient/GQL.js
@@ -2,7 +2,7 @@ import { gql } from "graphql-request";
 
 const DATA_AGENT = gql`
   query dataAgent($agentType: MasterEnum!, $agentId: Int) {
-    dataAgent(agentType: $agentType, agentId: $agentId) {
+    dataAgent(where: { agentType: $agentType, agentId: $agentId }) {
       id
       email
       firstName
