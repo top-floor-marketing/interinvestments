cbf45dee1efb0a99e112e29e33225e5f6870ce33
Msocram16 <marcos.ee16@gmail.com>
2024-11-26 12:11:27 -0600

    fixs numberFormatter quickSearch

--- Quick_search_Plugin_React/src/utils/FormaterNumber.js
+++ Quick_search_Plugin_React/src/utils/FormaterNumber.js
@@ -1,23 +1,21 @@
 const numFormatter = (num) => {
-    if (num > 999 && num < 1000000) {
-        // convert to K for number from > 1000 < 1 million 
-        return {
-            number: parseInt((num / 1000).toFixed(1)),
-            tag: 'K'
-        }
-    } else if (num > 1000000) {
-        // convert to M for number from > 1 million 
-        return {
-            number: parseInt((num / 1000000).toFixed(1)),
-            tag: 'M'
-        }
-    } else if (num < 1000) {
-        // if value < 1000, nothing to do
-        return {
-            number: num,
-            tag: ""
-        }
-    }
-}
+  if (num > 999 && num < 1000000) {
+    // convert to K for number from > 1000 < 1 million
+    return {
+      number: parseInt((num / 1000).toFixed(1)),
+      tag: "K",
+    };
+  } else if (num > 999999) {
+    // convert to M for number from > 1 million
+    return {
+      number: parseInt((num / 1000000).toFixed(1)),
+      tag: "M",
+    };
+  }
+  return {
+    number: num,
+    tag: "",
+  };
+};
 
-export default numFormatter
+export default numFormatter;
