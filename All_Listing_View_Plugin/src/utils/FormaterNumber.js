26a8d49e06d7318fb698fb9bd8372a494b863daf
Msocram16 <marcos.ee16@gmail.com>
2024-11-26 12:08:15 -0600

    fixs numFormatter all_listing

--- All_Listing_View_Plugin/src/utils/FormaterNumber.js
+++ All_Listing_View_Plugin/src/utils/FormaterNumber.js
@@ -5,18 +5,16 @@ const numFormatter = (num) => {
           number: parseInt((num / 1000).toFixed(1)),
           tag: 'K'
       }
-  } else if (num > 1000000) {
+  } else if (num > 999999) {
       // convert to M for number from > 1 million 
       return {
           number: parseInt((num / 1000000).toFixed(1)),
           tag: 'M'
       }
-  } else if (num < 1000) {
-      // if value < 1000, nothing to do
-      return {
-          number: num,
-          tag: ""
-      }
+  }
+  return {
+      number: num,
+      tag: ""
   }
 }
 
