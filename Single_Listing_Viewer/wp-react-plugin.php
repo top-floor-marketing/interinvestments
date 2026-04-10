926725c69a99e567e8a8cf9bc1462017f685eca3
Miguel Espinales <mee@topfloormarketing.net>
2026-04-10 14:21:01 -0600

    fix: actualizar la versión del plugin a 0.0.3 y mejorar la formateación del código

--- Single_Listing_Viewer/wp-react-plugin.php
+++ Single_Listing_Viewer/wp-react-plugin.php
@@ -2,56 +2,61 @@
 /*
 Plugin Name: Single_Listing
 Description: Use the [Single_Listing] shortcode to display the plugin
-Version: 0.0.1
+Version: 0.0.3
 Author: Topfloormarketing
 Author URI: https://www.topfloormarketing.net/
 */
 
-class Single_Listing {
+class Single_Listing
+{
 
-    protected $plugin_options_page = '';
+  protected $plugin_options_page = '';
 
-    /**
-     * Class constructor
-     */
-    public function __construct() {
-      require('plugin_options.php');
-    }
+  /**
+   * Class constructor
+   */
+  public function __construct()
+  {
+    require('plugin_options.php');
+  }
 
-    /**
-     * Initialize hooks.
-     */
-    public function init() {
+  /**
+   * Initialize hooks.
+   */
+  public function init()
+  {
 
-      add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_frontend_scripts' ) );
-    }
+    add_action('wp_enqueue_scripts', array($this, 'enqueue_frontend_scripts'));
+  }
 
-    public function enqueue_frontend_scripts($hook) {
+  public function enqueue_frontend_scripts($hook)
+  {
 
-      //wp_enqueue_script('react');
-      //wp_enqueue_script('react-dom');
-      
-      // add react and react-dom from core
-      $dep = ''; //['wp-element'];
-    }
+    //wp_enqueue_script('react');
+    //wp_enqueue_script('react-dom');
+
+    // add react and react-dom from core
+    $dep = ''; //['wp-element'];
+  }
 }
 
 $Single_Listing = new Single_Listing();
 $Single_Listing->init();
 
-function Single_Listing_shortcode( $atts ) {
+function Single_Listing_shortcode($atts)
+{
   $handle = 'blog-view-wp-react-plugin-';
 
   // enqueue development or production React code
-  if(file_exists(dirname(__FILE__) . "/dist/static/js/main.js")) {
+  if (file_exists(dirname(__FILE__) . "/dist/static/js/main.js")) {
     $handle .= 'prod';
-    wp_enqueue_script( $handle, plugins_url( "/dist/static/js/main.js", __FILE__ ), ['wp-element'], '0.1', true );
-    wp_enqueue_style( $handle, plugins_url( "/dist/static/css/main.css", __FILE__ ), false, '0.1', 'all' );
+    wp_enqueue_script($handle, plugins_url("/dist/static/js/main.js", __FILE__), ['wp-element'], '0.1', true);
+    wp_enqueue_style($handle, plugins_url("/dist/static/css/main.css", __FILE__), false, '0.1', 'all');
   } else {
     $handle .= 'dev';
-    wp_enqueue_script( $handle, 'http://localhost:3000/static/js/bundle.js', ['wp-element'], '0.1', true );
+    wp_enqueue_script($handle, 'http://localhost:3000/static/js/bundle.js', ['wp-element'], '0.1', true);
   }
   return "<div id='Single_Listing'></div>";
 }
 
-add_shortcode( 'Single_Listing', 'Single_Listing_shortcode' );
+add_shortcode('Single_Listing', 'Single_Listing_shortcode');
